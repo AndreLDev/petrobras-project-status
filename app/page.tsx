c063d5f146ceb39a082bbff1c63674f43bcc0ae5
andreluiz.cardozo9@gmail.com <andreluiz.cardozo9@gmail.com>
2024-01-31 03:09:39 -0300

    home page, and view modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,86 @@
-import Image from 'next/image';
 import NavBar from '@/components/navbar';
-import SearchedTable from '@/components/searchedTable';
+import { FiBriefcase, FiSettings, FiPhone } from 'react-icons/fi';
 
 export default function Home() {
   return (
-    <header>
+    <>
       <NavBar />
-    </header>
+      <div className="h-screen bg-green-100">
+        <header className="bg-green-500 shadow-md p-4">
+          <div className="container mx-auto">
+            <h1 className="text-4xl font-bold text-white">Bem-vindo à PSC</h1>
+          </div>
+        </header>
+        <section className="container mx-auto my-8 p-8 bg-white rounded-lg shadow-md">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="mb-8">
+              <img
+                src="https://imagens.ebc.com.br/YDtjVOuxvS99bgBK-VzLV_Hsh8c=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/_mg_1446_0.jpg?itok=cAwV_9g3"
+                alt="Imagem de destaque"
+                className="rounded-lg"
+              />
+            </div>
+            <div className="col-span-2">
+              <p className="text-xl text-gray-700 mb-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus
+                ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus.
+                Quisque nonummy ipsum non arcu. Sed in libero ut nibh placerat accumsan.
+              </p>
+              <div className="flex justify-end">
+                <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+                  Saiba Mais
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md">
+          <h2 className="text-3xl font-bold mb-6 text-gray-800">Nossos Serviços</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <ServiceCard
+              title="Serviço 1"
+              description="Descrição detalhada do Serviço 1. Inclua informações importantes e destaque os benefícios para os clientes."
+              icon={<FiBriefcase className="text-3xl mb-4 text-gray-800" />}
+            />
+            <ServiceCard
+              title="Serviço 2"
+              description="Descrição detalhada do Serviço 2. Destaque os recursos exclusivos e como podem ajudar os clientes."
+              icon={<FiSettings className="text-3xl mb-4 text-gray-800" />}
+            />
+            <ServiceCard 
+              title="Serviço 3"
+              description="Descrição detalhada do Serviço 3. Forneça informações claras e atraentes sobre o que oferece."
+              icon={<FiPhone className="text-3xl mb-4 text-gray-800" />}
+            />
+          </div>
+        </section>
+        <section className="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md">
+          <h2 className="text-3xl font-bold mb-6 text-gray-800">Entre em Contato</h2>
+          <p className="text-xl text-gray-700 mb-6">
+            Estamos aqui para ajudar! Entre em contato conosco para obter mais informações sobre
+            nossos serviços ou para esclarecer qualquer dúvida.
+          </p>
+          <div className="flex justify-center">
+            <button className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">
+              Contato
+            </button>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
+
+interface ServiceCardProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, icon }) => (
+  <div className="p-6 bg-white rounded-md shadow-md transition-transform transform hover:scale-105">
+    {icon}
+    <h3 className="text-xl font-semibold mb-2 text-gray-800">{title}</h3>
+    <p className="text-gray-700">{description}</p>
+  </div>
+);
--- components/searchedTable.jsx
+++ components/searchedTable.jsx
@@ -16,27 +16,31 @@ import {
   DropdownItem,
   Chip,
   Pagination,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  getKeyValue,
 } from "@nextui-org/react";
 import { PlusIcon } from "../public/PlusIcon";
 import { VerticalDotsIcon } from "../public/VerticalDotsIcon";
 import { SearchIcon } from "../public/SearchIcon";
 import { ChevronDownIcon } from "../public/ChevronDownIcon";
-import { columns, contracts, statusOptions, typeOptions } from "../data";
+import { columns, contracts, phaseOptions, typeOptions, columns2 } from "../data";
 import { capitalize } from "../utils";
 
-const statusColorMap = {
-  active: "success",
-  paused: "danger",
-  vacation: "warning",
-};
 
-const INITIAL_VISIBLE_COLUMNS = ["name", "title", "summary", "status", "lastUpdated", "actions"];
+const INITIAL_VISIBLE_COLUMNS = ["name", "title", "status", "lastUpdated", "actions"];
 
 export default function SearchedTable() {
+  const [modalContractId, setModalContractId] = React.useState(null);
+  const [contractId, setContractId] = React.useState(0);
   const [filterValue, setFilterValue] = React.useState("");
   const [selectedKeys, setSelectedKeys] = React.useState(new Set([]));
   const [visibleColumns, setVisibleColumns] = React.useState(new Set(INITIAL_VISIBLE_COLUMNS));
-  const [statusFilter, setStatusFilter] = React.useState("all");
+  const [phaseFilter, setPhaseFilter] = React.useState("all");
   const [typesFilter, setTypesFilter] = React.useState("all");
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [sortDescriptor, setSortDescriptor] = React.useState({
@@ -45,7 +49,14 @@ export default function SearchedTable() {
   });
   const [page, setPage] = React.useState(1);
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const hasSearchFilter = Boolean(filterValue);
+
+  const onOpenContractModal = React.useCallback((contractId) => {
+    setModalContractId(contractId);
+    onOpen();
+  }, [onOpen]);
 
   const headerColumns = React.useMemo(() => {
     if (visibleColumns === "all") return columns;
@@ -61,9 +72,9 @@ export default function SearchedTable() {
         contract.name.toLowerCase().includes(filterValue.toLowerCase()),
       );
     }
-    if (statusFilter !== "all" && Array.from(statusFilter).length !== statusOptions.length) {
+    if (phaseFilter !== "all" && Array.from(phaseFilter).length !== phaseOptions.length) {
       filteredContracts = filteredContracts.filter((contract) =>
-        Array.from(statusFilter).includes(contract.status),
+        Array.from(phaseFilter).includes(contract.phase),
       );
     }
     if (typesFilter !== "all" && Array.from(typesFilter).length !== typeOptions.length) {
@@ -73,7 +84,7 @@ export default function SearchedTable() {
     }
 
     return filteredContracts;
-  }, [contracts, filterValue, statusFilter, typesFilter]);
+  }, [contracts, filterValue, phaseFilter, typesFilter]);
 
   const pages = Math.ceil(filteredItems.length / rowsPerPage);
 
@@ -100,19 +111,24 @@ export default function SearchedTable() {
     switch (columnKey) {
       case "name":
         return (
-          <h3>{cellValue}</h3>
+          <h3 className="text-xs sm:text-base" >{cellValue}</h3>
         );
       case "title":
         return (
           <div className="flex flex-col">
-            <p className="text-bold text-small capitalize">{cellValue}</p>
-            <p className="text-bold text-tiny capitalize text-default-400">{contract.team}</p>
+            <p className="text-xs sm:text-base text-bold capitalize">{cellValue}</p>
           </div>
         );
+      case "summary":
+      return (
+        <div className="flex flex-col">
+          <p className="text-xs sm:text-base text-bold capitalize">{cellValue}</p>
+        </div>
+      );
       case "status":
         return (
-          <Chip className="capitalize" color={statusColorMap[contract.status]} size="sm" variant="flat">
-            {cellValue}
+          <Chip className="text-xs sm:text-base capitalize" color={contract.statusColor} size="sm" variant="flat">
+            {contract.phase}
           </Chip>
         );
       case "actions":
@@ -125,7 +141,7 @@ export default function SearchedTable() {
                 </Button>
               </DropdownTrigger>
               <DropdownMenu className="text-black">
-                <DropdownItem>Visualizar</DropdownItem>
+                <DropdownItem onPress={() => onOpenContractModal(contract.id)}>Visualizar</DropdownItem>
                 <DropdownItem>Editar</DropdownItem>
                 <DropdownItem>Excluir</DropdownItem>
               </DropdownMenu>
@@ -174,17 +190,18 @@ export default function SearchedTable() {
         <div className="flex justify-between gap-3 items-end">
           <Input
             isClearable
-            className="w-full sm:max-w-[44%]"
+            className="w-full sm:max-w-[44%] text-xs sm:text-base"
             placeholder="Search by name..."
             startContent={<SearchIcon />}
             value={filterValue}
             onClear={() => onClear()}
             onValueChange={onSearchChange}
+            size="xs sm:full"
           />
           <div className="flex gap-3">
             <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
-                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+              <DropdownTrigger className="sm:flex">
+                <Button className="text-xs sm:text-base" endContent={<ChevronDownIcon className="text-small" />} variant="flat">
                   Tipo
                 </Button>
               </DropdownTrigger>
@@ -195,7 +212,7 @@ export default function SearchedTable() {
                 selectedKeys={typesFilter}
                 selectionMode="multiple"
                 onSelectionChange={setTypesFilter}
-                className="text-black"
+                className="text-black "
               >
                 {typeOptions.map((type) => (
                   <DropdownItem key={type.uid} className="capitalize">
@@ -214,14 +231,14 @@ export default function SearchedTable() {
                 disallowEmptySelection
                 aria-label="Table Columns"
                 closeOnSelect={false}
-                selectedKeys={statusFilter}
+                selectedKeys={phaseFilter}
                 selectionMode="multiple"
-                onSelectionChange={setStatusFilter}
+                onSelectionChange={setPhaseFilter}
                 className="text-black"
               >
-                {statusOptions.map((status) => (
-                  <DropdownItem key={status.uid} className="capitalize">
-                    {capitalize(status.name)}
+                {phaseOptions.map((phase) => (
+                  <DropdownItem key={phase.uid} className="capitalize">
+                    {capitalize(phase.name)}
                   </DropdownItem>
                 ))}
               </DropdownMenu>
@@ -248,7 +265,7 @@ export default function SearchedTable() {
                 ))}
               </DropdownMenu>
             </Dropdown>
-            <Button color="success" endContent={<PlusIcon />}>
+            <Button className="text-xs sm:text-base" color="success" endContent={<PlusIcon />}>
               Add New
             </Button>
           </div>
@@ -271,7 +288,7 @@ export default function SearchedTable() {
     );
   }, [
     filterValue,
-    statusFilter,
+    phaseFilter,
     typesFilter,
     visibleColumns,
     onRowsPerPageChange,
@@ -293,7 +310,7 @@ export default function SearchedTable() {
           total={pages}
           onChange={setPage}
         />
-        <div className="hidden sm:flex w-[30%] justify-end gap-2">
+        <div className="flex sm:flex w-[30%] justify-end gap-2">
           <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onPreviousPage}>
             Previous
           </Button>
@@ -306,37 +323,75 @@ export default function SearchedTable() {
   }, [selectedKeys, items.length, page, pages, hasSearchFilter]);
 
   return (
-    <Table
-      aria-label="Example table with custom cells, pagination and sorting"
-      isHeaderSticky
-      bottomContent={bottomContent}
-      bottomContentPlacement="outside"
-      classNames={{
-        wrapper: "max-h-[382px]",
-      }}
-      sortDescriptor={sortDescriptor}
-      topContent={topContent}
-      topContentPlacement="outside"
-      onSortChange={setSortDescriptor}
-    >
-      <TableHeader columns={headerColumns}>
-        {(column) => (
-          <TableColumn
-            key={column.uid}
-            align={column.uid === "actions" ? "center" : "start"}
-            allowsSorting={column.sortable}
-          >
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      <TableBody emptyContent={"No users found"} items={sortedItems}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+    <>
+      <Table
+        aria-label="Example table with custom cells, pagination and sorting"
+        isHeaderSticky
+        bottomContent={bottomContent}
+        bottomContentPlacement="outside"
+        classNames={{
+          wrapper: "max-h-[382px]",
+        }}
+        sortDescriptor={sortDescriptor}
+        topContent={topContent}
+        topContentPlacement="outside"
+        onSortChange={setSortDescriptor}
+      >
+        <TableHeader columns={headerColumns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+              allowsSorting={column.sortable}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody emptyContent={"No users found"} items={sortedItems}>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+
+
+      <Modal style={{ maxHeight: '99vh', overflowY: 'auto' }} size={"3xl"} isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className=" text-black flex flex-col sm:flex-row justify-between items-center">{contracts[modalContractId - 1]?.name}</ModalHeader>
+              <ModalBody className="text-black" >
+                <p className="text-xs sm:text-base">Título: {contracts[modalContractId - 1]?.title}</p>
+                <p className="text-xs sm:text-base">Síntese: {contracts[modalContractId - 1]?.summary}</p>
+                <Table aria-label="Example table with dynamic content">
+                  <TableHeader columns={columns2}>
+                    {(column) => <TableColumn key={column.uid}>{column.name}</TableColumn>}
+                  </TableHeader>
+                  <TableBody items={contracts[modalContractId - 1]?.statusHistory}>
+                    {(item) => (
+                      <TableRow className="text-xs sm:text-base" key={item.key}>
+                        {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+
+
+
   );
 }
